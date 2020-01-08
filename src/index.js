00d9510bdd11993d78560cbfb5706198f55e5a00
kiki <772765729@qq.com>
2020-01-08 14:46:05 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+
+const Board = function () {
+
+  return (
+    <div></div>
+  )
+};
+
+ReactDOM.render(<Board />, document.getElementById("root"));
